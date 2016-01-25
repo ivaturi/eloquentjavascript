b49869cb07d0204390ddd25893daf09212e24c72
abhi <abhi@mailbox.org>
2016-01-25 05:44:53 +0100

    Chapter 5 - exercise 1 : flattening arrays

---
+++ chap05/exercise1_flattening.js
@@ -0,0 +1,14 @@
+/*
+  Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the input arrays.
+*/
+
+var arrays = [
+    [1,2,3,4,5],
+    ["This","is","a"],
+    "string"
+];
+
+var reduced_array = arrays.reduce(function(i,j){
+  return i.concat(j);
+});
+console.log(reduced_array);
